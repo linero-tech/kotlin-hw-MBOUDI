728aa498a75ed3915ebb70058b7ecd06d2b11099
B0UDI <M0URHAF.MM@GMAIL.COM>
2023-02-07 19:08:53 +0100

    task 10

--- src/main/kotlin/lms_126/Task10.kt
+++ src/main/kotlin/lms_126/Task10.kt
@@ -2,5 +2,17 @@ package lms_126
 
 fun task10(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        INPUT email
+        IF email not valid 
+        OUTPUT email error
+        INPUT password
+        IF password less than 6 charachter 
+        OUTPUT password error
+        ELSE
+        Create the account
+        
+    """
+    }
+
 }
