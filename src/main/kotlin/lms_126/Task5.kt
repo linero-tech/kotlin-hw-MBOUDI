a3ad739d2d18549bd6301144ca84a7cc356ce36e
B0UDI <M0URHAF.MM@GMAIL.COM>
2023-02-07 06:18:58 +0100

    task 5

--- src/main/kotlin/lms_126/Task5.kt
+++ src/main/kotlin/lms_126/Task5.kt
@@ -2,5 +2,16 @@ package lms_126
 
 fun task5(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return
+    val X = 7
+
+    when (X) {
+        1 -> println("Monday")
+        2 -> println("Tuesday")
+        3 -> println("Wednesday")
+        4 -> println("Thursday")
+        5 -> println("Friday")
+        6 -> println("Saturday")
+        7 -> println("Sunday")
+        else -> println("Invalid input")
 }
