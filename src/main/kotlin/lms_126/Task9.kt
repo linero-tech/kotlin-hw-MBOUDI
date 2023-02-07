ff768947f01fa1cb8e58c47bdae7166b21fed3a3
B0UDI <M0URHAF.MM@GMAIL.COM>
2023-02-07 19:17:28 +0100

    11 and 9

--- src/main/kotlin/lms_126/Task9.kt
+++ src/main/kotlin/lms_126/Task9.kt
@@ -2,5 +2,13 @@ package lms_126
 
 fun task9(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        INPUT master card to machine
+        
+        
+        
+        
+        
+        
+    """
 }
