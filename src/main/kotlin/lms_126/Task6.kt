50892306457c9698e65848eaf1ec5dba1df3850f
B0UDI <M0URHAF.MM@GMAIL.COM>
2023-02-07 06:29:32 +0100

    task 6

--- src/main/kotlin/lms_126/Task6.kt
+++ src/main/kotlin/lms_126/Task6.kt
@@ -2,5 +2,8 @@ package lms_126
 
 fun task6(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return
+    for (i in 1..100) {
+        if (i % 5 == 0) {
+            println(i)
 }
