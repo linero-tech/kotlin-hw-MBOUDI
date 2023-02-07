541b971810deb791f2c879aaef7f877f3d45b0d7
B0UDI <M0URHAF.MM@GMAIL.COM>
2023-02-07 06:52:49 +0100

    task 8

--- src/main/kotlin/lms_126/Task8.kt
+++ src/main/kotlin/lms_126/Task8.kt
@@ -2,5 +2,11 @@ package lms_126
 
 fun task8(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return val X = 15
+
+    when {
+        X % 3 == 0 && X % 5 == 0 -> println("FooBar")
+        X % 3 == 0 -> println("Foo")
+        X % 5 == 0 -> println("Bar")
+        else -> println("Qix")
 }
