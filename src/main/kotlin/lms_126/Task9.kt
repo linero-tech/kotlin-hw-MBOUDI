00e98d9475ce1439c41a518f413443ae7c884da0
B0UDI <M0URHAF.MM@GMAIL.COM>
2023-02-08 20:01:08 +0100

    Update Task9.kt

--- src/main/kotlin/lms_126/Task9.kt
+++ src/main/kotlin/lms_126/Task9.kt
@@ -4,11 +4,16 @@ fun task9(): String {
 
     return """
         INPUT master card to machine
-        
-        
-        
-        
-        
+        INPUT the correct code 
+        IF the cod correct
+          INPUT the amount to withdraws
+          IF the withdraws more than the balance 
+              PRINT try another amount 
+          ELSE draw the cash
+          OUTPUT the card
+          OUTPUT the receipt
+        ELSE 
+        INPUT try again or remove the card
         
     """
 }
