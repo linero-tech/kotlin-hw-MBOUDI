67670960a6d0f9937cb0f981b774aa6a861de800
B0UDI <M0URHAF.MM@GMAIL.COM>
2023-02-09 01:36:03 +0100

    Update Task11.kt

--- src/main/kotlin/lms_126/Task11.kt
+++ src/main/kotlin/lms_126/Task11.kt
@@ -3,24 +3,15 @@ package lms_126
 fun task11(): String {
 
     return """
-        
-        
-        
-        
-        
+        INPUT a temperature value and its unit
+        store the input in a variable
+        EXTRACT the temperature value from the input
+        IF the unit is C convert the temperature to F
+        IF the unit is F convert the temperature to C
+        IF the unit is neither C nor F print error
+        OUTPUT the result of the conversion
         
         """
 
-    //1. Prompt the user to enter a temperature value and its unit (e.g. 20C, 30C, 60F, 70F)
-    //2. Store the input in a variable
-    //3. Extract the temperature value from the input
-    //a. Remove the last character of the input
-    //b. Convert the remaining string to a double
-    //4. Extract the temperature unit from the input
-    //a. Take the last character of the input
-    //5. Check the temperature unit
-    //a. If the unit is 'C', convert the temperature to Fahrenheit
-    //b. If the unit is 'F', convert the temperature to Celsius
-    //c. If the unit is neither 'C' nor 'F', print an error message indicating an invalid temperature unit
-    //6. Print the result of the conversion
+
 }
