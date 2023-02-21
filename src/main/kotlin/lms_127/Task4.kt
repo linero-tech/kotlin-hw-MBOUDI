219062cf30cf60429b29a83f9b959c4bfdf09d7b
B0UDI <M0URHAF.MM@GMAIL.COM>
2023-02-21 19:11:05 +0100

    Update Task4.kt

--- src/main/kotlin/lms_127/Task4.kt
+++ src/main/kotlin/lms_127/Task4.kt
@@ -1,6 +1,11 @@
 package lms_127
 
 fun task4(base: Double, height: Double): Double {
+val result = 0.5 * base * height
+    return result
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+}
+fun main () {
+    println(task4(base = 1.0,  height = 1.0)) //0.5
+    println(task4(base = 5.0, height = 10.0)) //25.0
 }
