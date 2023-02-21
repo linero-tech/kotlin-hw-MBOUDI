f7231b61bbf1cd2ca51c6696c00788a95d554a49
B0UDI <M0URHAF.MM@GMAIL.COM>
2023-02-21 19:11:00 +0100

    Update Task3.kt

--- src/main/kotlin/lms_127/Task3.kt
+++ src/main/kotlin/lms_127/Task3.kt
@@ -1,6 +1,11 @@
 package lms_127
 
 fun task3(radius: Double): Double {
-
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+val result = 2.0 * radius * 3.1416
+    return result
+}
+fun main () {
+    println(task3(radius = 3.0)) //18.8496
+    println(task3(radius = 2.0)) //12.56
+    println(task3(radius = 5.0)) //31.416
 }
