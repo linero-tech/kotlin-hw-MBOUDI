e29b44b00ba31bdadf8f0a69edd2af7f0b2ff309
B0UDI <M0URHAF.MM@GMAIL.COM>
2023-02-22 02:53:56 +0100

    Update Task1.kt

--- src/main/kotlin/lms_127/Task1.kt
+++ src/main/kotlin/lms_127/Task1.kt
@@ -1,10 +1,10 @@
 package lms_127
 
 fun task1(): String {
-    val countryName = "Germany"
-    val capitalName = "Berlin"
-    val currencyName = "Euro"
-    val result = "The capital of $countryName is $capitalName and its currency is $currencyName"
+    val country = "Germany"
+    val capital = "Berlin"
+    val currency = "Euro"
+    val result = "The capital of $country is $capital and its currency is $currency"
 
     return result
 }
