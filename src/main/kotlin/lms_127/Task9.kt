32b5442507778404ae5883f79194f2629538607e
B0UDI <M0URHAF.MM@GMAIL.COM>
2023-02-22 03:21:53 +0100

    Update Task9.kt

--- src/main/kotlin/lms_127/Task9.kt
+++ src/main/kotlin/lms_127/Task9.kt
@@ -4,7 +4,7 @@ fun task9(sentence: String, character: Char): Boolean {
     return sentence.contains(character)
 }
 fun main (){
-    println(task9(sentence = "I code In KOTLIN", character ='i'))
+    println(task9(sentence = "I code In KOTLIN", character ='I'))
     println(task9(sentence = "I code In KOTLIN", character ='O'))
     println(task9(sentence = "I code In KOTLIN", character ='m'))
 }
