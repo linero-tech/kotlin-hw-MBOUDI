07e051357c73903f54ca1c8f49f86210a9a8a237
B0UDI <M0URHAF.MM@GMAIL.COM>
2023-03-01 03:17:15 +0100

    Update Task9.kt

--- src/main/kotlin/lms_127/Task9.kt
+++ src/main/kotlin/lms_127/Task9.kt
@@ -1,7 +1,7 @@
 package lms_127
 
 fun task9(sentence: String, character: Char): Boolean {
-    return sentence.contains(character)
+    return sentence.contains(character, ignoreCase = true)
 }
 fun main (){
     println(task9(sentence = "I code In KOTLIN", character ='I'))
