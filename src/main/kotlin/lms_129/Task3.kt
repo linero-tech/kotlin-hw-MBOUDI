7cce6f3246f8ffbffa9b7cce54347530c836a3be
B0UDI <M0URHAF.MM@GMAIL.COM>
2023-03-01 03:24:56 +0100

    Update Task3.kt

--- src/main/kotlin/lms_129/Task3.kt
+++ src/main/kotlin/lms_129/Task3.kt
@@ -1,6 +1,17 @@
 package lms_129
 
 fun task3(number: Int): Int {
+    var result = 1
+    for (I in 1 .. number) {
+        result *= I
+    }
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    return result
+}
+
+fun main () {
+
+    println(task3(number = 5))
+    println(task3(number = 3))
+    println(task3(number = 9))
 }
