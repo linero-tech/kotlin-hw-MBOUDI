e82b561d3f36ae40d22dcb8adce680d13d3500bb
B0UDI <M0URHAF.MM@GMAIL.COM>
2023-03-03 00:30:43 +0100

    Update Task1.kt

--- src/main/kotlin/lms_129/Task1.kt
+++ src/main/kotlin/lms_129/Task1.kt
@@ -1,6 +1,21 @@
 package lms_129
 
 fun task1(a: Int, b: Int): Int {
+    var result = 0
+    if (a >= b) {
+        result = 0
+    } else {
+        for (i in a .. b){
+            result += i
+        }
+    }
+    return result
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
 }
+fun main() {
+    val a = 5
+    val b = 5
+    val result = if (a >= b) 0 else (a..b).sum()
+    println("The sum of numbers between $a and $b is $result.")
+}
+
