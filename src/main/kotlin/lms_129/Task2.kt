8d99b5e8a20f06357d229aad0020b94019b16751
B0UDI <M0URHAF.MM@GMAIL.COM>
2023-03-03 01:41:01 +0100

    Update Task2.kt

--- src/main/kotlin/lms_129/Task2.kt
+++ src/main/kotlin/lms_129/Task2.kt
@@ -1,6 +1,18 @@
 package lms_129
 
 fun task2(number: Int): Boolean {
+    val number = (1..100).random()
+    var result = true
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    if (number <= 1){
+        result = false
+    }else {
+        for (i in 2 until number)
+            result = true
+    }
+    return result
+}
+fun main (){
+    val result = task2(number = 75)
+    println(result)
 }
