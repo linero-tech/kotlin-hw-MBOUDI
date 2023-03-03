01529e2c118077283e51e96bb86cd62d009b21d4
B0UDI <M0URHAF.MM@GMAIL.COM>
2023-03-03 02:00:55 +0100

    Update Task5.kt

--- src/main/kotlin/lms_129/Task5.kt
+++ src/main/kotlin/lms_129/Task5.kt
@@ -1,6 +1,14 @@
 package lms_129
 
 fun task5(sentence: String): Int {
-
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+var result = 0
+    for (char in sentence){
+        result++
+    }
+    return result
+}
+fun main (){
+    val sentence = "My teacher is smart in Kotlin"
+    val result = task5(sentence)
+    println("the number of character in the sentence is $result")
 }
