ea3d0ea27e3d29687e453d200c3c61369a139aa9
B0UDI <M0URHAF.MM@GMAIL.COM>
2023-03-06 20:11:49 +0100

    Update Task7.kt

--- src/main/kotlin/lms_129/Task7.kt
+++ src/main/kotlin/lms_129/Task7.kt
@@ -1,6 +1,14 @@
 package lms_129
 
 fun task7(a: Int, b: Int): Int {
-
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var result = 1
+    for (i in 1..b) {
+        result *= a
+    }
+    return result 
+}
+fun main (){
+    println(task7(a = 2, b = 3))
+    println(task7(a = 0, b = 0))
+    println(task7(a = 3, b = 3))
 }
