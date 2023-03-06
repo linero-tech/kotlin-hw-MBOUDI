5e75bcbdf312da0da079655a2b414fcbc1aed1ed
B0UDI <M0URHAF.MM@GMAIL.COM>
2023-03-06 20:11:44 +0100

    Update Task6.kt

--- src/main/kotlin/lms_129/Task6.kt
+++ src/main/kotlin/lms_129/Task6.kt
@@ -1,6 +1,23 @@
 package lms_129
 
 fun task6(number: Int): Int {
+    var reversedNumber = 0
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var num = number
+    while ( num > 0){
+        reversedNumber = reversedNumber * 10 + num % 10
+        num /=10
+    }
+    return reversedNumber
+}
+fun main() {
+    var number = 678
+    var reversedNumber = 0
+
+    while (number > 0) {
+        reversedNumber = reversedNumber * 10 + number % 10
+        number /= 10
+    }
+
+    println(reversedNumber)
 }
