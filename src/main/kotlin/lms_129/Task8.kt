ea6161fa53aae04e80e0c904a8017fd79841a644
B0UDI <M0URHAF.MM@GMAIL.COM>
2023-03-06 20:11:55 +0100

    Update Task8.kt

--- src/main/kotlin/lms_129/Task8.kt
+++ src/main/kotlin/lms_129/Task8.kt
@@ -1,6 +1,16 @@
 package lms_129
 
 fun task8(number: Int): Int {
+    var result = 0
+    var remainingNumber = number
+    while (remainingNumber > 0) {
+        result += remainingNumber % 10
+        remainingNumber /= 10
+    }
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    return result
+}
+fun main (){
+    println(task8(number = 123))
+    println(task8(number = 3123))
 }
