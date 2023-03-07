67b2e61dd9ad6353b04e6b177766f2a03dfff20c
B0UDI <M0URHAF.MM@GMAIL.COM>
2023-03-07 04:02:10 +0100

    Update Task4.kt

--- src/main/kotlin/lms_129/Task4.kt
+++ src/main/kotlin/lms_129/Task4.kt
@@ -1,14 +1,16 @@
 package lms_129
 
 fun task4(): Int {
-val result =0
+    var result = 0
     for (i in 1..1000) {
         if (i % 9 == 0) {
-            println(i)
+            result += i
         }
     }
     return result
 }
+
 fun main() {
-    println(task4())
+    val result = task4()
+    println(result)
 }
