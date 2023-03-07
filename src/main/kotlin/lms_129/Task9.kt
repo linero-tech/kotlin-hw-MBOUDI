b56b50d7d134073a6cc2a294ef2bb2d353f60d3e
B0UDI <M0URHAF.MM@GMAIL.COM>
2023-03-07 03:35:43 +0100

    Update Task9.kt

--- src/main/kotlin/lms_129/Task9.kt
+++ src/main/kotlin/lms_129/Task9.kt
@@ -4,13 +4,13 @@ fun task9(temperature: String): String {
     val regex = Regex("(\\-?\\d+)([CF])", RegexOption.IGNORE_CASE)
     val result = regex.find(temperature)?.let {
         val (value, unit) = it.destructured
-        "${if (unit.equals("C", ignoreCase = true)) value.toDouble() * 9 / 5 + 32 else (value.toDouble() - 32) * 5 / 9}${if (unit.equals("C", ignoreCase = true)) "F" else "C"}"
+        "${if (unit.equals("C", ignoreCase = true)) value.toInt() * 9 / 5 + 32 else (value.toInt() - 32) * 5 / 9}${if (unit.equals("C", ignoreCase = true)) "F" else "C"}"
     } ?: "Invalid temperature format"
     return result
 }
 
 fun main() {
     println(task9("50F"))
-    println(task9("-50f"))
+    println(task9("-23f"))
     println(task9("5c"))
 }
