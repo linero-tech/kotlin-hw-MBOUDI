491049446e7922ac3f1c7cd986a0a1b570702ca4
B0UDI <M0URHAF.MM@GMAIL.COM>
2023-03-07 03:25:28 +0100

    Update Task9.kt

--- src/main/kotlin/lms_129/Task9.kt
+++ src/main/kotlin/lms_129/Task9.kt
@@ -1,6 +1,16 @@
 package lms_129
 
 fun task9(temperature: String): String {
+    val regex = Regex("(\\-?\\d+)([CF])", RegexOption.IGNORE_CASE)
+    val result = regex.find(temperature)?.let {
+        val (value, unit) = it.destructured
+        "${if (unit.equals("C", ignoreCase = true)) value.toDouble() * 9 / 5 + 32 else (value.toDouble() - 32) * 5 / 9}${if (unit.equals("C", ignoreCase = true)) "F" else "C"}"
+    } ?: "Invalid temperature format"
+    return result
+}
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+fun main() {
+    println(task9("50F"))
+    println(task9("-50f"))
+    println(task9("5c"))
 }
