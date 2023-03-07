a6db5775c263e9a146f0f1cfad4eccee33f58ae3
B0UDI <M0URHAF.MM@GMAIL.COM>
2023-03-07 03:25:30 +0100

    Update Task10.kt

--- src/main/kotlin/lms_129/Task10.kt
+++ src/main/kotlin/lms_129/Task10.kt
@@ -2,5 +2,12 @@ package lms_129
 
 fun task10(password: String): Boolean {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val regex = Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#\$])[A-Za-z\\d@#\$]{6,10}$")
+    return regex.matches(password)
+}
+fun main (){
+    val password = "Kotlin123@"
+    val result = if (task10(password)) "valid" else "invalid"
+    println("password is $result")
+
 }
